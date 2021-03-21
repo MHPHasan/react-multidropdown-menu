5120182560ee059ada738da259b76ca917b115ee
PATWARY <md.hasan.69q@gmail.com>
2021-03-22 00:31:31 +0600

    initial commit

---
+++ src/pages/Reports.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export function Reports() {
+    return (
+        <div className="reports">
+            <h1>Reports</h1>
+        </div>
+    )
+}
+
+export function ReportsOne() {
+    return (
+        <div className="reports">
+            <h1>Reports/reports1</h1>
+        </div>
+    )
+}
+
+export function ReportsTwo() {
+    return (
+        <div className="reports">
+            <h1>Reports/reports2</h1>
+        </div>
+    )
+}
+
+export function ReportsThree() {
+    return (
+        <div className="reports">
+        <h1>Reports/reports3</h1>
+        </div>
+    )
+}
